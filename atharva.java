18d747e6d6662944b958287cfd3bf9996a63d033
atharva4704 <115482055+atharva4704@users.noreply.github.com>
2022-11-13 22:57:23 -0500

    Add files via upload

---
+++ atharva.java
@@ -0,0 +1,15 @@
+
+public class atharva{
+public static void main(String[] abcd){
+int a=30;
+int b=20;
+int c=40;
+int d=50;
+int e=60;
+
+int sub=5;
+
+int avg = (a+b+c+d+e)/sub;
+System.out.println ("Average is " +avg); 
+}
+}                                                       
